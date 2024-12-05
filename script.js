5fcc208c3f5a60f1b91eaf08815e72f065167f44
Ashutosh Kumar <ak2417942@gmail.com>
2024-12-05 22:43:55 +0530

    js DOM examples

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+    4 pillars of HTML Element
+    
+    --> Selection of an HTML element
+    --> Changing HTML
+    --> Changing CSS
+    --> Event listener 
+*/
+
+var myheadSelected = document.querySelector("h1"); // for selecting
+
+console.log(myheadSelected);
+
+myheadSelected.innerHTML = "My new heading !";
