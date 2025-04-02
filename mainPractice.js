79bbd0059b09368ea83c3b8c6ce9b089e7554078
Ashutosh Kumar <ak2417942@gmail.com>
2025-04-02 23:59:49 +0530

    added amaxing code which was exhausting my 1 core to nearly 100 perc capacity

---
+++ mainPractice.js
@@ -0,0 +1,26 @@
+/* 1. This is for practice js syntax 
+   2. Can be executed using node command 
+   3. Variables are hoisted in Javascript. 
+        To understand more on this understand how code are executed in Javascript.
+    
+*/
+
+// let firstName = "Ashutosh";
+// let lastName = "Kumar";
+// let a;
+
+// console.log(firstName + " " + lastName);
+
+// console.log(a); // this will print undefined means memory is assigned but variable is not initialized.
+
+/*
+
+If I run this below code . you can see in the activity monitor it will take ~ 99% to 100% memory. 
+let sum = 0;
+
+for( let i = 0; i <= 10000000000000 ; i++)
+     sum += i;
+
+console.log(sum);
+
+*/
